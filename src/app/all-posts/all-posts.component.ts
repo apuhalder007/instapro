8834e789ed928f0c6ca10087518776b77082bf88
apuhalder007 <apuplay007@gmail.com>
2019-05-28 20:35:47 +0530

    follower and favourite

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,13 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.css']
 })
 export class AllPostsComponent implements OnInit {
-  constructor() { }
+  private posts = [];
+  private uid: any;
+  private collectionPost = '/posts';
+  constructor(private router: Router) { }
   ngOnInit() {
+    let storageData = window.localStorage.getItem('loginUser');
+    let newstorageData = JSON.parse(storageData);
+    this.uid = newstorageData.uid;
+    this.getposts(10);
+  }
+
+  getposts(limit){
+    const dbref = firebase.database().ref(this.collectionPost);
+
+    dbref.limitToFirst(limit).on('child_added', (snapshot)=>{
+      this.posts.push({
+        key: snapshot.key,
+        data: snapshot.val()
+      });
+    })
+    console.log(this.posts);
+  }
+
+  makeFavourite(postId: any) {
+    console.log(postId);
+    if (this.uid && this.uid !== '') {
+      const dbref = firebase.database().ref('/Favourite');
+      dbref.push({
+        uid: this.uid,
+        postId: postId
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    } else {
+      this.router.navigate(['/auth/login']);
+    }
+  }
+
+  following(followId: any){
+    if (this.uid && this.uid !== '') {
+      const dbref = firebase.database().ref('/followers');
+      dbref.push({
+        uid: this.uid,
+        followId: followId
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    } else {
+      this.router.navigate(['/auth/login']);
+    }
   }
 
 }
