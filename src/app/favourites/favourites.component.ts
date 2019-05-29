35388bdac95a098971dc3d82a11e67858b10f8c3
apuhalder007 <apuplay007@gmail.com>
2019-05-29 19:43:00 +0530

    favourites, followers view generated

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.css']
 })
 export class FavouritesComponent implements OnInit {
-
+  private posts: any = [];
+  private uid: any;
+  private rootRef = firebase.database().ref();
   constructor() { }
 
   ngOnInit() {
+    let storageData = window.localStorage.getItem('loginUser');
+    let newstorageData = JSON.parse(storageData);
+    this.uid = newstorageData.uid;
+    this.getFavourites(10);
+  }
+
+  getFavourites(limit){
+    this.rootRef.child('Favourite').orderByChild('uid').equalTo(this.uid).
+    limitToFirst(limit).on('child_added', (snapshot)=>{
+      let postId = snapshot.val().postId;
+      this.rootRef.child(`posts/${postId}`).once('value', (postData)=>{
+        console.log(postData.val(), postData.val().imageUrl);
+        this.posts.push({
+          key: postData.key,
+          data: postData.val()
+        });
+      })
+    })
+    console.log(this.posts);
   }
 
 }
--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-followers',
   templateUrl: './followers.component.html',
   styleUrls: ['./followers.component.css']
 })
 export class FollowersComponent implements OnInit {
-
-  constructor() { }
+  private uid: any;
+  private rootRef: any = firebase.database().ref();
+  private users: any = [];
+  constructor() {
+  }
 
   ngOnInit() {
+    const storageData = window.localStorage.getItem('loginUser');
+    const newstorageData = JSON.parse(storageData);
+    this.uid = newstorageData.uid;
+    this.getFollowings(10);
+  }
+
+  getFollowings(limit){
+
+    this.rootRef.child('followers').orderByChild('uid').equalTo(this.uid).
+    limitToFirst(limit).on('child_added', (snapshot)=>{
+      console.log(snapshot.val());
+      this.rootRef.child('users').orderByChild('uid').equalTo(snapshot.val().followId).on('child_added', (userData)=>{
+        console.log(userData);
+        this.users.push({
+          key: userData.key,
+          data: userData.val()
+        });
+      })
+    })
   }
 
 }
